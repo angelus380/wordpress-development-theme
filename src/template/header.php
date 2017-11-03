8e01faf4551a4c43a109d75a41f917730e0ca8fd
Delmar Almeida <angelusdelmar@gmail.com>
2017-11-03 11:10:24 +0000

    adding wordpress build

---
+++ src/template/header.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width" >
+  <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+  <link href="<?php echo get_template_directory_uri(); ?>/img/icons/logo.png" rel="apple-touch-icon-precomposed">
+
+  <!-- erase before deploy -->
+  <meta name="robots" content="noindex,noimageindex,nofollow"/>
+  <!-- erase before deploy -->
+
+  <meta property="og:title" content="<?php bloginfo( 'name' ); ?>" />
+  <meta property="og:description" content="<?php bloginfo( 'description' ); ?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="<?php echo get_permalink(); ?>" />
+  <meta property="og:image" content="<?php echo get_template_directory_uri().'/screenshot.png';?>"/>
+
+	<title><?php bloginfo( 'name' ) ?></title>
+	
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
